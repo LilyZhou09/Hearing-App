64fac0ff620a01ad6f27bbc664eabe7a197d021b
LilyZhou09 <32224109+LilyZhou09@users.noreply.github.com>
2019-02-17 02:46:23 -0500

    Add files via upload

---
+++ TestForUI/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  TestForUI
+//
+//  Created by Lily Zhou on 2/16/19.
+//  Copyright © 2019 Lily Zhou. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import SwiftySound
+
+class ViewController: UIViewController {
+
+    @IBAction func action(_ sender: AnyObject){
+        performSegue(withIdentifier: "segueOne", sender: self)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func playBt(_ sender: Any) {
+        Sound.play(file: "everyone.wav")
+    }
+    
+    
+}
+
