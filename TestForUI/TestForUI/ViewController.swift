6a2971b44cb9c5489e74062052960b310379d539
LilyZhou09 <32224109+LilyZhou09@users.noreply.github.com>
2019-02-16 19:36:05 -0500

    Add files via upload

--- TestForUI/TestForUI/ViewController.swift
+++ TestForUI/TestForUI/ViewController.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var audioPlayer = AVAudioPlayer()
     @IBAction func action(_ sender: AnyObject){
         performSegue(withIdentifier: "segueOne", sender: self)
     }
@@ -17,8 +19,45 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        do{
+            audioPlayer = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "How", ofType: "mp3")!))
+                    
+            do {
+                try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+                try AVAudioSession.sharedInstance().setActive(true)
+            } catch {
+                print(error)
+            }
+            
+        }
+            
+        catch{
+            print("this is wrong")
+        }
     }
-
-
+    
+    @IBAction func Play(_ sender: Any) {
+        audioPlayer.play()
+    }
+    
+    @IBAction func Pause(_ sender: Any) {
+        if audioPlayer.isPlaying{
+            audioPlayer.pause()
+        }
+        else{
+            
+        }
+    }
+    
+    @IBAction func Replay(_ sender: Any) {
+        if audioPlayer.isPlaying{
+            audioPlayer.currentTime = 0
+            audioPlayer.play()
+        }
+        else{
+            audioPlayer.play()
+        }
+    }
+    
 }
 
